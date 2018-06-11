52bd619615225fb28e5ccc8b20759b18c40d1fbc
John Porto <john.porto@rocketmail.com>
2018-06-10 17:22:46 -0700

    completed the first section.

--- oop.js
+++ oop.js
@@ -46,25 +46,54 @@
 
 //2.
 
-function Person(name, city, age) {
-    this.name = name;
-    this.city = city;
-    this.age = age;
-}
-//3
+// function Person(name, city, age) {
+//     this.name = name;
+//     this.city = city;
+//     this.age = age;
+// }
+// //3
 
-Person.prototype.sayHey = function() {
-    console.log('Hey! My name is ' + this.name + ' I am ' + this.age + ' years old and live in ' + this.city + '.');
-}
+// Person.prototype.sayHey = function() {
+//     console.log('Hey! My name is ' + this.name + ' I am ' + this.age + ' years old and live in ' + this.city + '.');
+// }
 
-//4
+// //4
+
+// let p1 = new Person ('Paul', 'Oahu', 35);
+// let p2 = new Person ('Andrew', 'Bristol', 35);
+// let p3 = new Person ('Mike', 'Huntington Beach', 35);
+// let p4 = new Person ('Frank', 'Riverside', 35);
+// let p5 = new Person ('Bobby', 'Irvine', 35);
+
+
+// p1.sayHey();
+// p2.sayHey();
+// p3.sayHey();
+// p4.sayHey();
+// p5.sayHey();
+
+
+
+
+//5
+
+class Person {
+    constructor(name, city, age) {
+        this.name = name;
+        this.city = city;
+        this.age = age;
+    }
+
+    sayHey(){
+        console.log(`Hey! my name is ${this.name}. I am ${this.age} old and live in ${this.city}.`);
+    }    
+}
 
 let p1 = new Person ('Paul', 'Oahu', 35);
-let p2 = new Person ('Andrew', 'Bristol', 35);
-let p3 = new Person ('Mike', 'Huntington Beach', 35);
-let p4 = new Person ('Frank', 'Riverside', 35);
-let p5 = new Person ('Bobby', 'Irvine', 35);
-
+let p2 = new Person ('Andrew', 'Bristol', 40);
+let p3 = new Person ('Mike', 'Huntington Beach', 32);
+let p4 = new Person ('Frank', 'Riverside', 41);
+let p5 = new Person ('Bobby', 'Irvine', 25);
 
 p1.sayHey();
 p2.sayHey();
